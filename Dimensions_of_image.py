a05b2c3559991480ff55ec6840b253b311f06f29
simranbajaj27 <57626762+simranbajaj27@users.noreply.github.com>
2019-12-31 12:42:14 +0530

    Update Dimensions_of_image.py

--- Dimensions_of_image.py
+++ Dimensions_of_image.py
@@ -1,1 +1,11 @@
-
+import imutils
+import cv2
+ 
+# images are represented as a NumPy array
+# shape =[no. rows (height) , no. columns (width) ,no. channels (depth)]
+image = cv2.imread("jp.png")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+ 
+cv2.imshow("Image", image)
+cv2.waitKey(0

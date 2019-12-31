ee710b3a8efa4d7249ac810083028ea2fd97ed3e
simranbajaj27 <57626762+simranbajaj27@users.noreply.github.com>
2019-12-31 12:29:04 +0530

    Update Opening,showing and saving images.py

--- Opening,showing and saving images.py
+++ Opening,showing and saving images.py
@@ -1,1 +1,12 @@
+# import the necessary packages
+import cv2
+ 
+# load the input image 
+image = cv2.imread("flower.jpg")
 
+#save the image 
+cv2.imwrite('flower_new',image)
+
+# display the image to the screen 
+cv2.imshow("Image", image)
+cv2.waitKey(0)
